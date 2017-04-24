9962d6b72b763e3a8e8092fa9d6df734bd2d8e9b
Sean Farhat <seanfarhat@slo-76-131.ResHall.Berkeley.EDU>
2017-04-24 00:28:47 -0700

    Updated design, cleaned up sections, added arrow/scroll navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
 
+	var headingWidth = $('#heading-container').width();
+	var pageWidth = $(document).width();
+	var adjusted = (pageWidth - headingWidth) / 2;
+	$('heading-container').css('left', adjusted);
+
+	var numApplicationClicked = 0;
 	$('.check-circle').click(function() {
 		$(this).css('background-color', 'darkseagreen');
+		numApplicationClicked += 1;
+
+		if (numApplicationClicked == 3) {
+			$('#end-of-application').hide();
+			$('#end-of-application').text('Ah, perfect! We are here to help.');
+			$('#end-of-application').show();
+		}
 	});
+
+	$('.arrow-circle').click(function() {
+		if ($(this).hasClass('rotated')) {
+			$(this).removeClass('rotated');
+			$('html, body').animate({ scrollTop: 250 }, 2000);
+		} else {
+			$(this).addClass('rotated');
+			$('html, body').animate({ scrollTop: 750 }, 2000);
+		}
+	});
+
 });
