c5999c357c3416c3a28c4c85d5aff41cc936b07b
Sean Farhat <seanfarhat@slo-76-131.ResHall.Berkeley.EDU>
2017-04-25 13:22:00 -0700

    Improved auto-scrolling and finishing cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,26 +9,27 @@ $(document).ready(function() {
 			$('#end-of-application').hide();
 			$('#end-of-application').text('Ah, perfect! We are here to help.');
 			$('#end-of-application').show();
+			$('html, body').animate({ scrollTop: 300 }, 1500)
 		}
 	});
 
 	$('#section-1.arrow-circle').click(function() {
 		if ($(this).hasClass('rotated')) {
 			$(this).removeClass('rotated');
-			$('html, body').animate({ scrollTop: 250 }, 1500);
+			$('html, body').animate({ scrollTop: 300 }, 1500);
 		} else {
 			$(this).addClass('rotated');
-			$('html, body').animate({ scrollTop: 750 }, 1500);
+			$('html, body').animate({ scrollTop: 835 }, 1500);
 		}
 	});
 
 	$('#section-2.arrow-circle').click(function() {
 		if ($(this).hasClass('rotated')) {
 			$(this).removeClass('rotated');
-			$('html, body').animate({ scrollTop: 870 }, 1500);
+			$('html, body').animate({ scrollTop: 740 }, 1500);
 		} else {
 			$(this).addClass('rotated');
-			$('html, body').animate({ scrollTop: 1500 }, 1500);
+			$('html, body').animate({ scrollTop: 1350 }, 1500);
 		}
 	});
 
@@ -36,10 +37,12 @@ $(document).ready(function() {
 		var word = $(this).attr('id');
 		var card = '.card-container#' + word;
 		$(card).fadeIn(300);
+		$('#overlay').fadeIn(300);
 	});
 
 	$('.back-button').click(function() {
 		$('.card-container').fadeOut(300);
+		$('#overlay').fadeOut(300);
 	});
 
 });
